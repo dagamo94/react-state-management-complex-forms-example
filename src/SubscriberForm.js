5dbb440895c36856544d9e103aab9c8f4141b548
dan <da.ga.mo.94@gmail.com>
2022-03-17 00:54:18 -0600

    update SubscriberForm with more form elements and state keys

--- src/SubscriberForm.js
+++ src/SubscriberForm.js
@@ -1,51 +1,121 @@
-import {useState} from "react";
+import { useState } from "react";
 
 function SubscriberForm() {
-    const initialFormState = {
-      name: "",
-      email: "",
-    };
-    const [formData, setFormData] = useState({ ...initialFormState });
-    const handleChange = ({ target }) => {
-      setFormData({
-        ...formData,
-        [target.name]: target.value,
-      });
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log("Submitted:", formData);
-      setFormData({ ...initialFormState });
-    };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">
-          Enter Your Name:
-          <input
-            id="name"
-            type="text"
-            name="name"
-            onChange={handleChange}
-            value={formData.name}
-          />
-        </label>
-        <br />
-        <label htmlFor="email">
-          Your Email:
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={handleChange}
-            value={formData.email}
-          />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-    );
-  }
+  const initialFormState = {
+    name: "",
+    email: "",
+    referral: "",
+    age: "",
+    subscription: false,
+  };
+  const [formData, setFormData] = useState({ ...initialFormState });
+  const handleChange = ({ target }) => {
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    setFormData({
+      ...formData,
+      [target.name]: value,
+    });
+  };
 
-  export default SubscriberForm;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted:", formData);
+    setFormData({ ...initialFormState });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">
+        Enter Your Name:
+        <input
+          id="name"
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
+      </label>
+      <br />
+      <label htmlFor="email">
+        Your Email:
+        <input
+          id="email"
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
+        />
+      </label>
+      <br />
+      <label htmlFor="referral">
+        How did you hear about us?
+        <select
+          id="referral"
+          name="referral"
+          onChange={handleChange}
+          value={formData.referral}
+        >
+          <option value="">-- Select an Option --</option>
+          <option value="twitter">Twitter</option>
+          <option value="wom">Word of Mouth</option>
+          <option value="youtube">YouTube</option>
+        </select>
+      </label>
+      <br />
+      <fieldset>
+        <legend>How old are you?</legend>
+        <label htmlFor="low">
+          Under 18
+          <input
+            id="low"
+            type="radio"
+            name="age"
+            onChange={handleChange}
+            value="low"
+            checked={formData.age === "low"}
+          />
+        </label>
+        <br />
+        <label htmlFor="middle">
+          18 - 60
+          <input
+            id="middle"
+            type="radio"
+            name="age"
+            onChange={handleChange}
+            value="middle"
+            checked={formData.age === "middle"}
+          />
+        </label>
+        <br />
+        <label htmlFor="high">
+          Over 60
+          <input
+            id="high"
+            type="radio"
+            name="age"
+            onChange={handleChange}
+            value="high"
+            checked={formData.age === "high"}
+          />
+        </label>
+      </fieldset>
+      <br />
+      <label htmlFor="subscription">
+        Receive email notifications?
+        <input
+          id="subscription"
+          type="checkbox"
+          name="subscription"
+          onChange={handleChange}
+          checked={formData.subscription}
+          value="subscription"
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default SubscriberForm;
